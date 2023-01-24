6f97e60576110de786f35fe4776b585f36c4ff00
Oxyloon <oxyloon.quenos@gmail.com>
2023-01-24 15:30:24 +0100

    modif bonus +2 clicks

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var audio2 = new Audio('styles/tnt_explosion.mp3');
 var audio3 = new Audio('styles/big_tnt_explosion.mp3');
 
 // Variable permettant de lister les différentes images 
-var images = ['styles/dirt.png', 'styles/stone.png', 'styles/sandstone.png', 'styles/stonebricks.png'];
+var images = ['styles/dirt.png', 'styles/stone.png', 'styles/sandstone.png', 'styles/stonebricks.png', 'styles/deepslate_diamond.png'];
 
 // Initialisation du compteur d'images
 var counter = 0;
@@ -30,13 +30,13 @@ function clics() {
         clicks +=1;
         document.getElementById('clicks').innerHTML = "Nombre de clics : " + clicks;
                 //au bout de 10 clics = 1 block
-            if (clicks >= 64) {
+            if (clicks >= 32) {
                 counter ++;
                     blocksTT +=1;
                     blocks +=1;
                     document.getElementById('blocksTT').innerHTML = "Nombre de blocks detruits : " + blocksTT;
-                    clicks -=64;
-                    document.getElementById("main-image").src=images[counter %4]
+                    clicks -=32;
+                    document.getElementById("main-image").src=images[counter %5]
                 //au bout de 5 bloc detruit = 1 bonus
             } if (blocks >= 5) {
                 bonus +=1;
@@ -44,12 +44,12 @@ function clics() {
                 blocks -=5;
             }
             if (bonusactive >=1){
-                console.log("Le bonus x2 est activé ")
+                console.log("Le bonus +2 est activé ")
                 clicks +=1;
             }
 }
 
-function bonus1() { // x2 a chaque clics    
+function bonus1() { // +2 a chaque clicks    
     if (bonus >=2) {
             bonus -=2;
                 document.getElementById('bonus').innerHTML = "Pts disponible :  " + bonus;
