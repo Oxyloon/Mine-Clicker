dc634fc9ff6ec31552dfc34cc55c906ab0fdb968
quentin delos <q.delos@P210.sn>
2023-01-27 09:30:55 +0100

    réorganisation du code + affichage nb de bonusclics

--- script.js
+++ script.js
@@ -1,83 +1,92 @@
-// Clicks et le nombre de clicks au total
 let clicks = 0;
-let clicksTT = 0;
-
-// Blocks et le nombre de blocks au total
+let clicksTotal = 0;
 let blocks = 0;
-let blocksTT = 0;
-
+let blocksTotal = 0;
 let bonus = 0;
 let bonusclicks = 0;
-// Les audios du bonus 2 et 3
+let clicksbonus = 1;
+let counter = 0;
+
 let audio1 = new Audio('styles/anvil.mp3');
 let audio2 = new Audio('styles/tnt_explosion.mp3');
 let audio3 = new Audio('styles/big_tnt_explosion.mp3');
 
-// Variable permettant de lister les différentes images 
 let images = ['styles/dirt.png', 'styles/stone.png', 'styles/sandstone.png', 'styles/stonebricks.png', 'styles/deepslate_diamond.png'];
-
-// Initialisation du compteur d'images
-let counter = 0;
-
-// Selection de la div ou se trouve l'image 
 let img = document.querySelector("main");
 
+
+
+
 function clics() {
-    //nombre de clics total
-    clicksTT +=1;
-    document.getElementById('clicksTT').innerHTML = "Nombre de clics au total : " + clicksTT;
-    //passage aux clics par blocs
+    clicksTotal +=1;
+    document.getElementById('clicksTotal').innerHTML = "Nombre de clics au total : " + clicksTotal;
         clicks +=1;
         document.getElementById('clicks').innerHTML = "Nombre de clics : " + clicks;
-                //au bout de 10 clics = 1 block
-            if (clicks >= 32) {
-                counter ++;
-                    blocksTT +=1;
-                    blocks +=1;
-                    document.getElementById('blocksTT').innerHTML = "Nombre de blocks detruits : " + blocksTT;
-                    clicks -=32;
-                    document.getElementById("main-image").src=images[counter %5]
-                //au bout de 5 bloc detruit = 1 bonus
-            } if (blocks >= 5) {
-                bonus +=1;
-                document.getElementById('bonus').innerHTML = "Pts disponible :  " + bonus;
-                blocks -=5;
-            }
-            if (bonusclicks >=1){
-                clicks += bonusclicks;
-                console.log(bonusclicks); //affiche dans la console le nombre de bonus de clicks en plus
-            }
+            
+        if (clicks >= 32) 
+        {
+        counter ++;
+        blocksTotal +=1;
+        blocks +=1;
+            document.getElementById('blocksTotal').innerHTML = "Nombre de blocks detruits : " + blocksTotal;
+        clicks -=32;
+            document.getElementById("main-image").src=images[counter %5]
+        } 
+
+        if (blocks >= 5) 
+        {
+        bonus +=1;
+            document.getElementById('bonus').innerHTML = "Pts disponible :  " + bonus;
+        blocks -=5;
+        } 
+
+        if (bonusclicks >=1)
+        {
+        clicks += bonusclicks;
+        }
 }
 
-function bonus1() { // +2 a chaque clicks    
+
+
+
+function bonus1() { 
     if (bonus >=2) {
             bonus -=2;
             bonusclicks +=1;
+            clicksbonus +=1;
                 document.getElementById('bonus').innerHTML = "Pts disponible :  " + bonus;
+                document.getElementById('clicksbonus').innerHTML = "Bonus actuel : +" + clicksbonus;
                 audio1.play();
     } else {
         alert("Tu n'as pas assez de points bonus !");
     }
 }
 
-function bonus2() { // +1 000 blocks detruits
+
+
+
+function bonus2() {
     if (bonus >=5) {
         audio2.play();
             bonus -=5;
-            blocksTT +=1000;
+            blocksTotal +=1000;
                 document.getElementById('bonus').innerHTML = "Pts disponible :  " + bonus;
-                document.getElementById('blocksTT').innerHTML = "Nombre de blocks detruits : " + blocksTT;
+                document.getElementById('blocksTotal').innerHTML = "Nombre de blocks detruits : " + blocksTotal;
     } else {
         alert("Tu n'as pas assez de points bonus !");
     }
 }
-function bonus3() { // +10 000 blocks detruits
+
+
+
+
+function bonus3() {
     if (bonus >=10) {
         audio3.play();
             bonus -=10;
-            blocksTT +=10000;
+            blocksTotal +=10000;
                 document.getElementById('bonus').innerHTML = "Pts disponible :  " + bonus;
-                document.getElementById('blocksTT').innerHTML = "Nombre de blocks detruits : " + blocksTT;
+                document.getElementById('blocksTotal').innerHTML = "Nombre de blocks detruits : " + blocksTotal;
     } else {
         alert("Tu n'as pas assez de points bonus !");
     }
