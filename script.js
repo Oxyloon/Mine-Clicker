c36a53504473b6a7e23d24810c3ca4902f0c664f
Oxyloon <oxyloon.quenos@gmail.com>
2023-02-10 15:48:36 +0100

    alert box v2

--- script.js
+++ script.js
@@ -169,21 +169,16 @@ function saveStorage(){
     localStorage.setItem('clicksAdd',clicksAdd);
 }
 
-
-
-
-
-
-
-var ALERT_TITLE = "Oops!";
-var ALERT_BUTTON_TEXT = "Ok";
+// Variable Alert-Box
+let ALERT_TITLE = "Oops!";
+let ALERT_BUTTON_TEXT = "Ok";
 
 if(document.getElementById) {
     window.alert = function(txt) {
         createCustomAlert(txt);
     }
 }
-
+// change Alert Box
 function createCustomAlert(txt) {
     d = document;
 
@@ -195,7 +190,7 @@ function createCustomAlert(txt) {
 
     alertObj = mObj.appendChild(d.createElement("div"));
     alertObj.id = "alertBox";
-    if(d.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+    if(d.all && !window.chrome) alertObj.style.top = document.documentElement.scrollTop + "px";
     alertObj.style.left = (d.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
     alertObj.style.visiblity="visible";
 
@@ -203,18 +198,16 @@ function createCustomAlert(txt) {
     h1.appendChild(d.createTextNode(ALERT_TITLE));
 
     msg = alertObj.appendChild(d.createElement("p"));
-    //msg.appendChild(d.createTextNode(txt));
     msg.innerHTML = txt;
 
-    btn = alertObj.appendChild(d.createElement("a"));
-    btn.id = "closeBtn";
-    btn.appendChild(d.createTextNode(ALERT_BUTTON_TEXT));
-    btn.href = "#";
-    btn.focus();
-    btn.onclick = function() { removeCustomAlert();return false; }
+        btn = alertObj.appendChild(d.createElement("a"));
+        btn.id = "closeBtn";
+        btn.appendChild(d.createTextNode(ALERT_BUTTON_TEXT));
+        btn.href = "#";
+        btn.focus();
+        btn.onclick = function() { removeCustomAlert();return false; }
 
     alertObj.style.display = "block";
-
 }
 function removeCustomAlert() {
     document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));
